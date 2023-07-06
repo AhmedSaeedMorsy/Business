dbe59caa64117ff2bdd3a4065768093337ef8bb0
Ahmed Saeed Morsy <ahmedsaidmorsy74@gmail.com>
2023-07-06 11:38:48 +0300

    Edit support chat screen

--- lib/presentation/support_chat/view/support_chat_screen.dart
+++ lib/presentation/support_chat/view/support_chat_screen.dart
@@ -41,100 +41,98 @@ class SupportChatScreen extends StatelessWidget {
             ],
           ),
         ),
-        child: Expanded(
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              vertical: MediaQuery.of(context).size.height / AppSize.s30,
-              horizontal: MediaQuery.of(context).size.width / AppSize.s10,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  AppStrings.howCanWeHelp.tr(),
-                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                        fontSize: FontSizeManager.s26.sp,
-                      ),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height / AppSize.s40,
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(
-                    vertical: MediaQuery.of(context).size.height / AppSize.s50,
-                    horizontal: MediaQuery.of(context).size.width / AppSize.s40,
-                  ),
-                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                  decoration: BoxDecoration(
-                    color: ColorManager.white,
-                    borderRadius: BorderRadius.circular(
-                      AppSize.s10.w,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: MediaQuery.of(context).size.height / AppSize.s30,
+            horizontal: MediaQuery.of(context).size.width / AppSize.s10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                AppStrings.howCanWeHelp.tr(),
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      fontSize: FontSizeManager.s26.sp,
                     ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / AppSize.s40,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(
+                  vertical: MediaQuery.of(context).size.height / AppSize.s50,
+                  horizontal: MediaQuery.of(context).size.width / AppSize.s40,
+                ),
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                decoration: BoxDecoration(
+                  color: ColorManager.white,
+                  borderRadius: BorderRadius.circular(
+                    AppSize.s10.w,
                   ),
-                  child: Column(
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          Navigator.pushNamed(
-                            context,
-                            Routes.messagesRoute,
-                          );
-                        },
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              AppStrings.messages.tr(),
+                ),
+                child: Column(
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.pushNamed(
+                          context,
+                          Routes.messagesRoute,
+                        );
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            AppStrings.messages.tr(),
+                            style: Theme.of(context).textTheme.displayLarge,
+                          ),
+                          Icon(
+                            Icons.message,
+                            size: AppSize.s16.w,
+                            color: ColorManager.primaryColorBlue,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical:
+                            MediaQuery.of(context).size.height / AppSize.s40,
+                      ),
+                      child: Container(
+                        height: AppSize.s1.h,
+                        width: double.infinity,
+                        color: ColorManager.grey,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.pushNamed(
+                          context,
+                          Routes.chatWithSupportRoute,
+                        );
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Text(
+                              AppStrings.sendUsAMessage.tr(),
                               style: Theme.of(context).textTheme.displayLarge,
                             ),
-                            Icon(
-                              Icons.message,
-                              size: AppSize.s16.w,
-                              color: ColorManager.primaryColorBlue,
-                            ),
-                          ],
-                        ),
+                          ),
+                          Icon(
+                            Icons.send,
+                            size: AppSize.s16.w,
+                            color: ColorManager.primaryColorBlue,
+                          ),
+                        ],
                       ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                          vertical:
-                              MediaQuery.of(context).size.height / AppSize.s40,
-                        ),
-                        child: Container(
-                          height: AppSize.s1.h,
-                          width: double.infinity,
-                          color: ColorManager.grey,
-                        ),
-                      ),
-                      InkWell(
-                        onTap: () {
-                          Navigator.pushNamed(
-                            context,
-                            Routes.chatWithSupportRoute,
-                          );
-                        },
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Expanded(
-                              child: Text(
-                                AppStrings.sendUsAMessage.tr(),
-                                style: Theme.of(context).textTheme.displayLarge,
-                              ),
-                            ),
-                            Icon(
-                              Icons.send,
-                              size: AppSize.s16.w,
-                              color: ColorManager.primaryColorBlue,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
